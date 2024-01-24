4648158a79487ba20df70fa2fe7d00d088802318
Dalkiel11 <c.degarriga@gmail.com>
2024-01-24 03:17:08 +0100

    GameManager creado

---
+++ Tower of Wrath/Assets/Carles/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+
+
+    [Header("===VARIABLES===")]
+    public GameObject winConditionTrigger;
+    public GameObject puertaWinConditon1;
+    public GameObject puertaWinCondition2;
+
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
